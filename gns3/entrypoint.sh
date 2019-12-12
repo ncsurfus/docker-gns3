d18f068e5f89082a76db2959454e3a6fd91cedb5
Nathan Surfus <ncsurfus@gmail.com>
2019-12-11 19:24:01 -0600

    Added config file workaround

--- gns3/entrypoint.sh
+++ gns3/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 /linux_amd64_goioulic | sed -n '2,3p' > /data/.iourc
 /docker.sh &
+
+# workaround until I fix this
+rm -rf /data/config.ini
+cp /config.ini /data/config.ini
+
 exec gns3server -A --config /data/config.ini
